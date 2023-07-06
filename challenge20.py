1fe1a522d93fe6ceaa47d51bcfd87067466f5260
jociah <jociah@gmail.com>
2023-07-06 17:35:54 +0000

    learning about dictionaries

---
+++ challenge20.py
@@ -0,0 +1,14 @@
+# create wordbank list
+
+wordbank = ["indentation", "spaces"]
+
+# create student list
+
+tlgstudents = ['Alvert', 'Anthony', 'Brenden', 'craig', 'Deja', 'Elihu'
+# add interger 4 to the wordbank
+
+# propmt user for number 0-20 to be sure convert to int
+
+# use number from user to slice list
+
+# print output
