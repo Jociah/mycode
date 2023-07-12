d4322971da7bb72d9d1a6d86426b1377a6886ecb
jociah <jociah@gmail.com>
2023-07-12 15:41:22 +0000

    sftp with username and password

---
+++ smallscript.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Author: Jociah
+
+This program prints some things to the screen
+"""
+
+def main():
+    """Our main program"""
+    mynote = 'print this string' # define mynote
+    print(mynote)                # display our string
+
+main()
+
